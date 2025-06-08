010c4b0c066b8758c39ea8799b7394aa42e181d5
mrmikoma <miko.lj.mattila@gmail.com>
2025-06-08 21:02:18 +0300

    tf deployment added backend

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -16,6 +16,9 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    force_path_style            = true
+    skip_s3_checksum            = true
+    skip_requesting_account_id  = true
   }
 }
 
