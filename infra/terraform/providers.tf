03f0db5d0e42583f5f9b018606642d7b100b87c5
mrmikoma <miko.lj.mattila@gmail.com>
2025-06-08 20:59:22 +0300

    tf deployment added backend

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -2,8 +2,20 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = ">= 1.45.0"
+      version = ">= 1.51.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "salko-website"
+    key    = "terraform.tfstate"
+    region = "eu-central"
+    endpoints = {
+      s3 = "https://hel1.your-objectstorage.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
   }
 }
 
