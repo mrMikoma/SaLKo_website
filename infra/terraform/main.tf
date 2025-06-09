586f141d7c1ddd44b11f482d9b2ef36cea9ab61d
mrmikoma <miko.lj.mattila@gmail.com>
2025-06-09 22:36:03 +0300

    testing ansible runs

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -36,7 +36,7 @@ module "salko" {
 #################################################################
 
 resource "github_actions_variable" "server-ip-salko-0" {
-  count = module.salko.server_ips["salko-0"] != "" ? 1 : 0
+  count         = length(module.salko.server_ips["salko-0"]) > 0 ? 1 : 0
   repository    = "SaLKo_website"
   variable_name = format("server-ip-%s", "salko-0")
   value         = module.salko.server_ips["salko-0"]
