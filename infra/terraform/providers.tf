4a5cae252b724cfbf13a6d8dbc2b94dff4309750
mrmikoma <miko.lj.mattila@gmail.com>
2025-06-09 17:38:39 +0300

    updated terraform backend

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -9,20 +9,21 @@ terraform {
   backend "s3" {
     bucket = "salko-website"
     key    = "terraform.tfstate"
-    region = "eu-central"
-    endpoints = {
-      s3 = "https://hel1.your-objectstorage.com"
-    }
+    region = "auto"
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true
-    skip_s3_checksum            = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+
+    endpoints = {
+      s3 = "https://b60de3b655609a784e6dff508195806d.r2.cloudflarestorage.com/salko-website"
+    }
   }
 }
 
 provider "hcloud" {
   token = var.hetzner_api_token
 }
-
