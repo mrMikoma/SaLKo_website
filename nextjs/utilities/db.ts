84415ba53f1036e7330b0d22e390d7a97935e2b0
mrmikoma <miko.lj.mattila@gmail.com>
2025-08-31 17:12:27 +0300

    fix db issues in non-prod

--- nextjs/utilities/db.ts
+++ nextjs/utilities/db.ts
@@ -3,8 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectionPool = new Pool({
-  connectionString: process.env.DATABASE_CONNECTION_STRING,
-});
+// Only create pool if we have a valid connection string and we're not in CI
+const shouldCreatePool =
+  process.env.DATABASE_CONNECTION_STRING && !process.env.CI;
+
+const connectionPool = shouldCreatePool
+  ? new Pool({
+      connectionString: process.env.DATABASE_CONNECTION_STRING,
+    })
+  : null;
 
 export default connectionPool;
