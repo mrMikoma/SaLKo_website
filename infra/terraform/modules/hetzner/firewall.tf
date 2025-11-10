c0f2f4da3f4bdaa9b8dbdeae43a16e2efae66a93
mrmikoma <miko.lj.mattila@gmail.com>
2025-11-10 12:19:16 +0200

    migrate server to another project

--- infra/terraform/modules/hetzner/firewall.tf
+++ infra/terraform/modules/hetzner/firewall.tf
@@ -3,19 +3,37 @@ resource "hcloud_firewall" "default" {
   labels = {}
 
   rule {
-    description     = "SSH"
+    description     = "Allow all TCP from private network"
     destination_ips = []
     direction       = "in"
-    port            = "22"
     protocol        = "tcp"
     source_ips = [
-      "0.0.0.0/0", # temp
-      #var.network_cidr
+      var.network_cidr,
     ]
   }
 
   rule {
-    description     = "proxy"
+    description     = "Allow all UDP from private network"
+    destination_ips = []
+    direction       = "in"
+    protocol        = "udp"
+    source_ips = [
+      var.network_cidr,
+    ]
+  }
+
+  rule {
+    description     = "Allow ICMP from private network"
+    destination_ips = []
+    direction       = "in"
+    protocol        = "icmp"
+    source_ips = [
+      var.network_cidr,
+    ]
+  }
+
+  rule {
+    description     = "Allow HTTP from anywhere"
     destination_ips = []
     direction       = "in"
     port            = "80"
@@ -26,7 +44,7 @@ resource "hcloud_firewall" "default" {
   }
 
   rule {
-    description     = "proxy"
+    description     = "Allow HTTPS from anywhere"
     destination_ips = []
     direction       = "in"
     port            = "443"
